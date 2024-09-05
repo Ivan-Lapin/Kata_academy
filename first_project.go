7493b7746cc08ab9ea24636e5410c5c4556f98cb
Ivan-Lapin <104844376+Ivan-Lapin@users.noreply.github.com>
2024-09-06 01:02:00 +0300

    Add files via upload

--- first_project.go
+++ first_project.go
@@ -1,7 +1,104 @@
+//КАЛЬКУЛЯТОР
+
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello Kata Academy")
+	var desert = make(map[string]int)
+	desert["I"] = 1
+	desert["II"] = 2
+	desert["III"] = 3
+	desert["IV"] = 4
+	desert["V"] = 5
+	desert["VI"] = 6
+	desert["VII"] = 7
+	desert["VIII"] = 8
+	desert["IX"] = 9
+	desert["X"] = 10
+
+Start:
+	fmt.Println("Введите выражение (через пробел) с помощью арабских или римских чисел (от 1 до 10) с операцией(+, -, /, *) ")
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
+	text = strings.TrimSpace(text) // Удаляем лишние пробелы и символы новой строки
+	slice := strings.Split(text, " ")
+
+	if len(slice) != 3 {
+		fmt.Println("Неверный ввод")
+		fmt.Println()
+		goto Start
+	}
+
+	firstelem := slice[0]
+	xi := 0
+	secondelem := slice[2]
+	yi := 0
+
+	flag := false
+	if val, ok := desert[firstelem]; ok {
+		flag = true
+		xi = val
+	}
+	if val, ok := desert[secondelem]; ok {
+		yi = val
+	} else {
+		flag = false
+	}
+
+	if !flag {
+		x, err1 := strconv.Atoi(slice[0])
+		if err1 != nil {
+			log.Fatal("Ошибка преобразования первого числа:", err1)
+		}
+		y, err2 := strconv.Atoi(slice[2])
+		if err2 != nil {
+			log.Fatal("Ошибка преобразования второго числа:", err2)
+		}
+
+		switch slice[1] {
+		case "+":
+			fmt.Println(x + y)
+		case "-":
+			fmt.Println(x - y)
+		case "*":
+			fmt.Println(x * y)
+		case "/":
+			if y == 0 {
+				fmt.Println("Ошибка: деление на ноль.")
+			} else {
+				fmt.Println(x / y)
+			}
+		default:
+			fmt.Println("Неизвестный оператор")
+		}
+	} else {
+		switch slice[1] {
+		case "+":
+			fmt.Println(xi + yi)
+		case "-":
+			if xi >= yi {
+				fmt.Println(xi - yi)
+			} else {
+				fmt.Println("Ошибка: результат отрицательный для римских чисел.")
+			}
+		case "*":
+			fmt.Println(xi * yi)
+		case "/":
+			if yi == 0 {
+				fmt.Println("Ошибка: деление на ноль.")
+			} else {
+				fmt.Println(xi / yi)
+			}
+		default:
+			fmt.Println("Неизвестный оператор")
+		}
+	}
 }
